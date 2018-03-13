864670366aade75e3dcac001dc5723941b5f78c4
ccskun <ccskun@foxmail.com>
2018-03-13 10:53:25 +0800

    创建

---
+++ src/com/ccskun/gof/bridge/DisplayImpl.java
@@ -0,0 +1,10 @@
+package com.ccskun.gof.bridge;
+
+/**
+ * Created by ccskun on 2018/3/11.
+ */
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+    public abstract void rawPrint();
+    public abstract void rawClose();
+}
